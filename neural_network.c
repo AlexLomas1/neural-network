693ab5e8eec9b80ba3f49b784152e7421f220ae8
Alex Lomas <alexlomas951@gmail.com>
2025-06-06 17:28:17 +0800

    Fixed memory leak, and created Layer struct to store the weights, biases, and number of nodes for each layer of neural net

--- neural_network.c
+++ neural_network.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include "matrix.h"
 
-Matrix forward_pass(Matrix* input, Matrix weights[], Matrix biases[], int layers) {
-    Matrix layer_in, layer_out;
+typedef struct Layer {
+    int nodes;
+    Matrix weights;
+    Matrix biases;
+} Layer;
+
+Matrix forward_pass(Matrix* input, Layer layers[], int num_layers) {
+    Matrix layer_in, temp, layer_out;
     layer_in = *input;
 
     printf("Input:\n");
@@ -10,18 +16,19 @@ Matrix forward_pass(Matrix* input, Matrix weights[], Matrix biases[], int layers
 
     // Simple feedforward process: each layer's output is calculated, and given to the next layer as 
     // input until the output layer is reached. 
-    for (int i=0; i < layers; i++) {
+    for (int i=0; i < num_layers; i++) {
         printf("\nWeights:\n");
-        display_matrix(&weights[i]);
+        display_matrix(&layers[i].weights);
         printf("\nBiases:\n");
-        display_matrix(&biases[i]);
+        display_matrix(&layers[i].biases);
 
         // The output, y, of each layer is calculated as y = wx + b, where x is the input matrix, w is
         // the weight matrix of the layer, and b is the bias matrix of the layer.
-        layer_out = matrix_multiplication(&weights[i], &layer_in);
-        layer_out = matrix_addition(&layer_out, &biases[i]);
+        temp = matrix_multiplication(&layers[i].weights, &layer_in);
+        layer_out = matrix_addition(&temp, &layers[i].biases);
+        free_matrix(&temp);
 
-        if (i != (layers-1)) {
+        if (i != (num_layers-1)) {
             printf("\nLayer output:\n");
         }
         else {
@@ -29,6 +36,8 @@ Matrix forward_pass(Matrix* input, Matrix weights[], Matrix biases[], int layers
         }
 
         display_matrix(&layer_out);
+
+        free_matrix(&layer_in);
         layer_in = layer_out;
     }
 
@@ -37,40 +46,40 @@ Matrix forward_pass(Matrix* input, Matrix weights[], Matrix biases[], int layers
 
 void main() {
     // Creation of matrices for preset weights, biases, and input.
-    Matrix weights[2];
-    weights[0] = create_matrix(2, 2);
-    set_element(&weights[0], 0, 0, 4);
-    set_element(&weights[0], 0, 1, 8);
-    set_element(&weights[0], 1, 0, 1);
-    set_element(&weights[0], 1, 1, 10);
+    Layer layers[2];
 
-    weights[1] = create_matrix(2, 2);
-    set_element(&weights[1], 0, 0, 5);
-    set_element(&weights[1], 0, 1, 2);
-    set_element(&weights[1], 1, 0, 9);
-    set_element(&weights[1], 1, 1, 14);
+    layers[0].weights = create_matrix(2, 2);
+    set_element(&layers[0].weights, 0, 0, 4);
+    set_element(&layers[0].weights, 0, 1, 8);
+    set_element(&layers[0].weights, 1, 0, 1);
+    set_element(&layers[0].weights, 1, 1, 10);
 
-    Matrix biases[2];
-    biases[0] = create_matrix(2, 1);
-    set_element(&biases[0], 0, 0, 1);
-    set_element(&biases[0], 1, 0, 4);
+    layers[0].biases = create_matrix(2, 1);
+    set_element(&layers[0].biases, 0, 0, 1);
+    set_element(&layers[0].biases, 1, 0, 4);
 
-    biases[1] = create_matrix(2, 1);
-    set_element(&biases[1], 0, 0, 5);
-    set_element(&biases[1], 1, 0, 0);
+    layers[1].weights = create_matrix(2, 2);
+    set_element(&layers[1].weights, 0, 0, 5);
+    set_element(&layers[1].weights, 0, 1, 2);
+    set_element(&layers[1].weights, 1, 0, 9);
+    set_element(&layers[1].weights, 1, 1, 14);
+
+    layers[1].biases = create_matrix(2, 1);
+    set_element(&layers[1].biases, 0, 0, 5);
+    set_element(&layers[1].biases, 1, 0, 0);
 
     Matrix input = create_matrix(2, 1);
     set_element(&input, 0, 0, 3);
     set_element(&input, 1, 0, 2);
 
-    int layers = 2; // Stores the number of hidden layers, does not count the input and output layers.
-    Matrix output = forward_pass(&input, weights, biases, layers);
+    int num_layers = 2; // Stores the number of non-input layers.
+    Matrix output = forward_pass(&input, layers, num_layers);
 
     // Freeing memory allocated to matrices.
     free_matrix(&input);
-    free_matrix(&weights[0]);
-    free_matrix(&weights[1]);
-    free_matrix(&biases[0]);
-    free_matrix(&biases[1]);
+    free_matrix(&layers[0].weights);
+    free_matrix(&layers[0].biases);
+    free_matrix(&layers[1].weights);
+    free_matrix(&layers[1].biases);
     free_matrix(&output);
 }
