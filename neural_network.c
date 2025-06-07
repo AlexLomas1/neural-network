5f133004c9e547cad06b47be2a7e0e6b41d5e677
Alex Lomas <alexlomas951@gmail.com>
2025-06-07 23:02:24 +0800

    Created Network struct, and created functions to initialise a network, initialise a layer, and to free all memory allocations associated with a network

--- neural_network.c
+++ neural_network.c
@@ -1,13 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "matrix.h"
 
 typedef struct Layer {
-    int nodes;
     Matrix weights;
     Matrix biases;
+    int num_nodes;
 } Layer;
 
-Matrix forward_pass(Matrix* input, Layer layers[], int num_layers) {
+typedef struct Network {
+    Layer* layers;
+    int num_layers;
+} Network;
+
+Layer init_layer(int input_size, int output_size) {
+    // Initialises a layer with weight and bias matrices having all elements as zero.
+    Layer new_layer;
+    new_layer.weights = create_matrix(output_size, input_size);
+    new_layer.biases = create_matrix(output_size, 1);
+    new_layer.num_nodes = output_size;
+
+    return new_layer;
+}
+
+Network init_neural_net(int num_layers, int input_nodes, int layer_sizes[]) {
+    // Initialises a neural network with the given number of layers, and number of nodes for each layer.
+    Network new_network;
+    new_network.num_layers = num_layers;
+    new_network.layers = calloc(num_layers, sizeof(Layer));
+
+    int input_size;
+    for (int i=0; i < num_layers; i++) {
+        if (i==0) {
+            input_size = input_nodes;
+        }
+        else {
+            input_size = new_network.layers[i-1].num_nodes;
+        }
+
+        new_network.layers[i] = init_layer(input_size, layer_sizes[i]);
+    }
+
+    return new_network;
+}
+
+void free_network(Network* net) {
+    // Frees memory allocated to layers, weight matrices, and bias matrices in the network.
+    for (int i=0; i < net->num_layers; i++) {
+        free_matrix(&net->layers[i].weights);
+        free_matrix(&net->layers[i].biases);
+        net->layers[i].num_nodes = 0;
+    }
+
+    if (net->layers != NULL) {
+        free(net->layers);
+        net->layers = NULL;
+    }
+
+    net->num_layers = 0;
+}
+
+Matrix forward_pass(Network* net, Matrix* input) {
     Matrix layer_in, temp, layer_out;
     layer_in = *input;
 
@@ -16,19 +69,19 @@ Matrix forward_pass(Matrix* input, Layer layers[], int num_layers) {
 
     // Simple feedforward process: each layer's output is calculated, and given to the next layer as 
     // input until the output layer is reached. 
-    for (int i=0; i < num_layers; i++) {
+    for (int i=0; i < net->num_layers; i++) {
         printf("\nWeights:\n");
-        display_matrix(&layers[i].weights);
+        display_matrix(&net->layers[i].weights);
         printf("\nBiases:\n");
-        display_matrix(&layers[i].biases);
+        display_matrix(&net->layers[i].biases);
 
         // The output, y, of each layer is calculated as y = wx + b, where x is the input matrix, w is
         // the weight matrix of the layer, and b is the bias matrix of the layer.
-        temp = matrix_multiplication(&layers[i].weights, &layer_in);
-        layer_out = matrix_addition(&temp, &layers[i].biases);
+        temp = matrix_multiplication(&net->layers[i].weights, &layer_in);
+        layer_out = matrix_addition(&temp, &net->layers[i].biases);
         free_matrix(&temp);
 
-        if (i != (num_layers-1)) {
+        if (i != (net->num_layers-1)) {
             printf("\nLayer output:\n");
         }
         else {
@@ -41,45 +94,43 @@ Matrix forward_pass(Matrix* input, Layer layers[], int num_layers) {
         layer_in = layer_out;
     }
 
-    return layer_in; //  Returns the input to the output layer
+    return layer_out; 
 }
 
 void main() {
-    // Creation of matrices for preset weights, biases, and input.
-    Layer layers[2];
+    // Layer sizes and number of layers hardcoded for now. In future, these could be retrieved from a
+    // seperate file.
+    int layer_sizes[2];
+    layer_sizes[0] = 2;
+    layer_sizes[1] = 1;
+    Network test_net;
+    test_net = init_neural_net(2, 2, layer_sizes);
 
-    layers[0].weights = create_matrix(2, 2);
-    set_element(&layers[0].weights, 0, 0, 4);
-    set_element(&layers[0].weights, 0, 1, 8);
-    set_element(&layers[0].weights, 1, 0, 1);
-    set_element(&layers[0].weights, 1, 1, 10);
+    // Setting preset weights and biases.
+    set_element(&test_net.layers[0].weights, 0, 0, 4);
+    set_element(&test_net.layers[0].weights, 0, 1, 8);
+    set_element(&test_net.layers[0].weights, 1, 0, 1);
+    set_element(&test_net.layers[0].weights, 1, 1, 10);
 
-    layers[0].biases = create_matrix(2, 1);
-    set_element(&layers[0].biases, 0, 0, 1);
-    set_element(&layers[0].biases, 1, 0, 4);
+    set_element(&test_net.layers[0].biases, 0, 0, 1);
+    set_element(&test_net.layers[0].biases, 1, 0, 4);
 
-    layers[1].weights = create_matrix(2, 2);
-    set_element(&layers[1].weights, 0, 0, 5);
-    set_element(&layers[1].weights, 0, 1, 2);
-    set_element(&layers[1].weights, 1, 0, 9);
-    set_element(&layers[1].weights, 1, 1, 14);
+    set_element(&test_net.layers[1].weights, 0, 0, 5);
+    set_element(&test_net.layers[1].weights, 0, 1, 2);
 
-    layers[1].biases = create_matrix(2, 1);
-    set_element(&layers[1].biases, 0, 0, 5);
-    set_element(&layers[1].biases, 1, 0, 0);
+    set_element(&test_net.layers[1].biases, 0, 0, 5);
 
     Matrix input = create_matrix(2, 1);
-    set_element(&input, 0, 0, 3);
-    set_element(&input, 1, 0, 2);
+    double in1, in2;
+    printf("Enter two input values: ");
+    scanf("%lf %lf", &in1, &in2);
+    set_element(&input, 0, 0, in1);
+    set_element(&input, 1, 0, in2);
 
-    int num_layers = 2; // Stores the number of non-input layers.
-    Matrix output = forward_pass(&input, layers, num_layers);
+    Matrix output = forward_pass(&test_net, &input);
 
-    // Freeing memory allocated to matrices.
+    // Freeing allocated memory.
+    free_network(&test_net);
     free_matrix(&input);
-    free_matrix(&layers[0].weights);
-    free_matrix(&layers[0].biases);
-    free_matrix(&layers[1].weights);
-    free_matrix(&layers[1].biases);
     free_matrix(&output);
 }
