24ec3dd8e444a47b7b913efa84eb7158bce08b68
Alex Lomas <alexlomas951@gmail.com>
2025-06-21 11:01:18 +0800

    Implemented gradient descent, and a single training step (forward pass, loss calc, backward pass, and parameter updates)

--- src/nn/training.c
+++ src/nn/training.c
@@ -4,7 +4,7 @@
 #include "maths/loss.h"
 #include "nn/neural_network.h"
 
-void backpropagation(Network* net, const Matrix* input, const Matrix* y, const Matrix* y_pred, const LossFunc* loss_func) { 
+void backpropagation(Network* net, const Matrix* input, const Matrix* loss_deriv) { 
     Layer* output_layer= &net->layers[net->num_layers-1];
 
     // Freeing derivative matrices before overwriting
@@ -13,7 +13,7 @@ void backpropagation(Network* net, const Matrix* input, const Matrix* y, const M
     free_matrix(&output_layer->dL_db);
 
     // dL_dz = dL_da * da_dz
-    Matrix dL_da = loss_func->derivative_ptr(y, y_pred);
+    Matrix dL_da = copy_matrix(loss_deriv); // loss_func->derivative_ptr(y, y_pred)
     Matrix da_dz = copy_matrix(&output_layer->z);
     apply_func(&da_dz, output_layer->activation->derivative_ptr);
     output_layer->dL_dz = hadamard_product(&dL_da, &da_dz);
@@ -71,6 +71,49 @@ void backpropagation(Network* net, const Matrix* input, const Matrix* y, const M
     printf("Backpropagation completed, hopefully with no incompatable matrix dimensions errors\n");
 }
 
+void gradient_descent(Network* net, double learning_rate) {
+    // Updates the weights and biases of each layer based on gradients calculated from backpropagation
+    // and the learning rate.
+    for (int layer_count=0; layer_count < net->num_layers; layer_count++) {
+        Layer* curr_layer = &net->layers[layer_count];
+
+        Matrix diff = matrix_scalar_multiplication(&curr_layer->dL_dw, -learning_rate);
+        Matrix new_weights = matrix_addition(&curr_layer->weights, &diff);
+        free_matrix(&curr_layer->weights);
+        free_matrix(&diff);
+        curr_layer->weights = copy_matrix(&new_weights);
+
+        diff = matrix_scalar_multiplication(&curr_layer->dL_db, -learning_rate);
+        Matrix new_biases = matrix_addition(&curr_layer->biases, &diff);
+        free_matrix(&curr_layer->biases);
+        free_matrix(&diff);
+        curr_layer->biases = copy_matrix(&new_biases);
+
+        display_matrix(&curr_layer->weights);
+        printf("\n");
+        display_matrix(&curr_layer->biases);
+        printf("\n");
+    }
+}
+
+void train_step(Network* net, const Matrix* input, const Matrix* expected_output, const LossFunc* loss_func, double learning_rate) {
+    // Performs one training step: forward pass, loss calculation, backward pass, and parameter updates.
+    Matrix output = forward_pass(net, input);
+    printf("\n");
+    printf("Correct output:\n");
+    display_matrix(expected_output);
+    printf("\n");
+
+    double loss_val = loss_func->func_ptr(expected_output, &output);
+    printf("Loss: %f \n", loss_val);
+    Matrix loss_deriv = loss_func->derivative_ptr(expected_output, &output);
+    backpropagation(net, input, &loss_deriv);
+    free_matrix(&output);
+    free_matrix(&loss_deriv);
+
+    gradient_descent(net, learning_rate);
+}
+
 int main() { // test sequence for now, to be removed later
     // Layer sizes, number of layers, and activation functions hardcoded for now. In future, these could
     // be retrieved from a seperate file.
@@ -79,8 +122,8 @@ int main() { // test sequence for now, to be removed later
     layer_sizes[1] = 1;
 
     const ActivationFunc* activations[2];
-    activations[0] = &ReLu;
-    activations[1] = &sigmoid;
+    activations[0] = &tanh_custom;
+    activations[1] = &ReLu;
 
     Network test_net;
     test_net = init_neural_net(2, 2, layer_sizes, activations);
@@ -106,43 +149,15 @@ int main() { // test sequence for now, to be removed later
     set_element(&input, 0, 0, in1);
     set_element(&input, 1, 0, in2);
 
-    Matrix output = forward_pass(&test_net, &input);
-    printf("\n");
+    Matrix expected_output = create_matrix(1, 1);
+    set_element(&expected_output, 0, 0, 500);
 
-    Matrix y = create_matrix(output.rows, output.cols); // Intentionally set to all zero
-    printf("Correct output:\n");
-    display_matrix(&y);
-    printf("\n");
-
-    // Calculating losses and the gradient matrices of the losses with different loss functions.
-    double MSE_res = MSE.func_ptr(&y, &output);
-    printf("MSE: %f \n", MSE_res);
-    Matrix MSE_grad = MSE.derivative_ptr(&y, &output);
-    printf("MSE gradient matrix:\n");
-    display_matrix(&MSE_grad);
-    printf("\n");
-    free_matrix(&MSE_grad);
-
-    double MAE_res = MAE.func_ptr(&y, &output);
-    printf("MAE: %f \n", MAE_res);
-    Matrix MAE_grad = MAE.derivative_ptr(&y, &output);
-    printf("MAE gradient matrix:\n");
-    display_matrix(&MAE_grad);
-    printf("\n");
-    free_matrix(&MAE_grad);
-
-    double BCE_res = BCE.func_ptr(&y, &output);
-    printf("BCE: %f \n", BCE_res);
-    Matrix BCE_grad = BCE.derivative_ptr(&y, &output);
-    printf("BCE gradient matrix:\n");
-    display_matrix(&BCE_grad);
-    free_matrix(&BCE_grad);
-
-    backpropagation(&test_net, &input, &y, &output, &MSE);
+    for (int i=0; i < 20; i++) {
+        train_step(&test_net, &input, &expected_output, &MSE, 0.1);
+    }
 
     // Freeing allocated memory.
     free_network(&test_net);
     free_matrix(&input);
-    free_matrix(&output);
-    free_matrix(&y);
+    free_matrix(&expected_output);
 }
